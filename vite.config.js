51b46913239f357e6e337c7acd58758db4185f5e
narensill <166576124+narensill@users.noreply.github.com>
2025-01-09 23:43:20 +0530

    changes in vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,13 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    include: ['@ffmpeg/ffmpeg'], // Pre-bundle FFmpeg for optimization
+    include: ['@ffmpeg/ffmpeg', 'styled-components'], // Pre-bundle FFmpeg for optimization
   },
   build: {
     outDir: 'build', // Set the output directory for the build files
     emptyOutDir: true, // Clears the build folder before building
     rollupOptions: {
-      external: ['jszip'], // Mark jszip as an external module
+      external: ['jszip' , 'styled-components'], // Mark jszip as an external module
       output: {
         format: 'es', // Ensure ES module format for compatibility
       },
