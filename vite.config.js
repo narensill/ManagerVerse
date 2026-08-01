72fadc507835bce374400b9b4897c475df22fb4a
narensill <166576124+narensill@users.noreply.github.com>
2026-08-02 04:08:41 +0530

    Changes to optimize development

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ['@ffmpeg/ffmpeg', 'styled-components'],
-  },
-  build: {
-    outDir: 'dist', // Change back to 'dist' as Vercel expects this
-    emptyOutDir: true,
-    rollupOptions: {
-      external: ['jszip', 'styled-components'],
-      output: {
-        format: 'es',
-      },
-    },
-  },
   resolve: {
     alias: {
-      '@components': '/src/components',
-      '@utils': '/src/utils',
+      "@components": "/src/components",
+      "@utils": "/src/utils",
     },
   },
 });
